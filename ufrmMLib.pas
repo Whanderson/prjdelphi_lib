497652a7f96578e99abf257260ee75119c6d9283
Whanderson <wsr@softgran.com.br>
2018-08-11 11:57:58 -0400

    commit lib

---
+++ ufrmMLib.pas
@@ -0,0 +1,24 @@
+unit ufrmMLib;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TfrmMLib = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmMLib: TfrmMLib;
+
+implementation
+
+{$R *.dfm}
+
+end.
